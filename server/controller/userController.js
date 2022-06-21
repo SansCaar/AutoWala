aa66db6854345434afb2e0a8bc157eb8c628cb54
Sushil Bhattarai <bhattaraisushil005@gmail.com>
2022-06-21 20:42:21 +0545

    server

--- server/controller/userController.js
+++
@@ -1,7 +0,0 @@
-import usersSchema from "../model/users";
-
-
-export const getAllUsers = () =>{
-
-    
-};
--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,8 +1,6 @@
 import express from "express";
-import { getAllUsers } from "../controller/userController";
 
 const router = express.Router();
 
-router.route("/").get(getAllUsers); // for getting all the users
 
 export default router;
