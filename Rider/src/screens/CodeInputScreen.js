e46837f3132d81ab4027db08bc5737c6facedbd2
Sanskar <sskrlmsl@gmail.com>
2022-07-01 10:17:44 +0545

    Passanger routing to ridescreen

--- Rider/src/screens/CodeInputScreen.js
+++ Rider/src/screens/CodeInputScreen.js
@@ -1,9 +1,16 @@
-import React,{useState} from "react";
-import { StyleSheet, Text, View, Image, Linking, Pressable } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Linking,
+  Pressable,
+} from "react-native";
 import CodePopup from "../components/CodePopup";
 import Header from "../components/Header";
 
-import { getRideData,cancelRide,completeRide } from "../context/api";
+import { getRideData, cancelRide, completeRide } from "../context/api";
 // const rideDetails = {
 //   title: "Your Ride",
 //   name: "Ram Prasad",
@@ -14,17 +21,17 @@ import { getRideData,cancelRide,completeRide } from "../context/api";
 //   phoneNumber: "+9779847000000",
 // };
 const CodeInputScreen = ({ navigation, route }) => {
-    const [rideDetails,setRideDetails]=useState({});
-    const [checkvalidation,setCheckValidation]=useState(false);
+  const [rideDetails, setRideDetails] = useState({});
+  const [checkvalidation, setCheckValidation] = useState(false);
 
   const { id } = route.params;
- const data = async ()=>{
-  const rideData = await  getRideData(id)
-  setRideDetails(rideData)
-  const {ride_validated}=rideData;
-setCheckValidation(ride_validated)
- }
-data();
+  const data = async () => {
+    const rideData = await getRideData(id);
+    setRideDetails(rideData);
+    const { ride_validated } = rideData;
+    setCheckValidation(ride_validated);
+  };
+  data();
   return (
     <View style={{ flex: 1 }}>
       <Image source={require("../../assets/map2.png")} style={styles.map} />
@@ -38,50 +45,61 @@ data();
       >
         <Header iconL="arrow-left" onPressL={navigation.goBack} />
 
-      
         <CodePopup data={rideDetails} />
-{checkvalidation?
-
-        <Pressable style={{
-          position:"absolute",
-          justifyContent:"center",
-          backgroundColor:"green",
-          width:120,
-          height:40,
-          borderRadius:20,
-          alignSelf:"center",
-          bottom:20,
-        }}><Text style={{
-          textAlign:"center",
-          color:"white"
-        }} onPress={async ()=>
-        
-        {
-          
-          var data = await completeRide(id)
-          const {ride_status}= data;
-          if(ride_status=='DRIVER_COMPLETED')
-          {
-            alert("Completed");
-            navigation.navigate("Home")
-          }
-        }
-          }>Completed</Text>
-        </Pressable>
-
-           :  <Pressable style={{
-          position:"absolute",
-          justifyContent:"center",
-          backgroundColor:"red",
-          width:120,
-          height:40,
-          borderRadius:20,
-          alignSelf:"center",
-          bottom:20,
-        }}><Text style={{
-          textAlign:"center",
-          color:"white"
-        }} onPress={()=>cancelRide(id)}>Cancel Ride</Text></Pressable> }
+        {checkvalidation ? (
+          <Pressable
+            style={{
+              position: "absolute",
+              justifyContent: "center",
+              backgroundColor: "green",
+              width: 120,
+              height: 40,
+              borderRadius: 20,
+              alignSelf: "center",
+              bottom: 20,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+              }}
+              onPress={async () => {
+                var data = await completeRide(id);
+                const { ride_status } = data;
+                if (ride_status == "DRIVER_COMPLETED") {
+                  alert("Completed");
+                  navigation.navigate("Home");
+                }
+              }}
+            >
+              Completed
+            </Text>
+          </Pressable>
+        ) : (
+          <Pressable
+            style={{
+              position: "absolute",
+              justifyContent: "center",
+              backgroundColor: "red",
+              width: 120,
+              height: 40,
+              borderRadius: 20,
+              alignSelf: "center",
+              bottom: 20,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+              }}
+              onPress={() => cancelRide(id)}
+            >
+              Cancel Ride
+            </Text>
+          </Pressable>
+        )}
       </View>
     </View>
   );
--- server/routes/reqride.js
+++ server/routes/reqride.js
@@ -1,24 +1,33 @@
 import express from "express";
-import {setRide,getAllRide,getRideById ,getValidRides,setAccepted,checkAccepted,driverCanceled,userCanceled,setValidate, driverCompleted} from '../controller/_reqrides.js';
+import {
+  setRide,
+  getAllRide,
+  getRideById,
+  getValidRides,
+  setAccepted,
+  checkAccepted,
+  driverCanceled,
+  userCanceled,
+  setValidate,
+  driverCompleted,
+} from "../controller/_reqrides.js";
 const reqriderouter = express.Router();
 
-reqriderouter.route("/").get(getAllRide)
-reqriderouter.route("/:id").get(getRideById)
-reqriderouter.route("/post").post(setRide)
-reqriderouter.route("/get/rides").get(getValidRides)
-reqriderouter.route("/accept/:id").get(setAccepted)
-reqriderouter.route("/check/:id").get(checkAccepted)
-reqriderouter.route("/drivercancel/:id").get(driverCanceled)
-reqriderouter.route("/drivercomplete/:id").get(driverCompleted)
-reqriderouter.route("/usercomplete/:id").get(driverCompleted)
+reqriderouter.route("/").get(getAllRide);
+reqriderouter.route("/:id").get(getRideById);
+reqriderouter.route("/post").post(setRide);
+reqriderouter.route("/get/rides").get(getValidRides);
+reqriderouter.route("/accept/:id").get(setAccepted);
+reqriderouter.route("/check/:id").get(checkAccepted);
+reqriderouter.route("/drivercancel/:id").get(driverCanceled);
+reqriderouter.route("/drivercomplete/:id").get(driverCompleted);
+reqriderouter.route("/usercomplete/:id").get(driverCompleted);
 
-
-reqriderouter.route("/usercancel/:id").get(userCanceled)
-reqriderouter.route("/accept/validate/:id").get(setValidate)
+reqriderouter.route("/usercancel/:id").get(userCanceled);
+reqriderouter.route("/accept/validate/:id").get(setValidate);
 
 // router.route("/:id").get(getOneUser)
 // router.route("/:id").delete(dltUser)
 // router.route("/:id").patch(updateUser)
 
 export default reqriderouter;
-               
